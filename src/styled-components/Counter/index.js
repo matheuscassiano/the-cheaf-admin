8e151ccfd770c77c114919a70c022d497b8a0b48
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-03-02 20:11:16 -0300

    feat: add counter function

--- src/styled-components/Counter/index.js
+++ src/styled-components/Counter/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CounterContainer, CounterButton } from './style';
 
 export default function Counter() {
+  const [number, setNumber] = useState(0);
   return (
     <CounterContainer>
-      <CounterButton>-</CounterButton>
-      <span>12</span>
-      <CounterButton>+</CounterButton>
+      <CounterButton onClick={() => setNumber(number - 1)}>-</CounterButton>
+      <span>{number}</span>
+      <CounterButton onClick={() => setNumber(number + 1)}>+</CounterButton>
     </CounterContainer>
   );
 }
