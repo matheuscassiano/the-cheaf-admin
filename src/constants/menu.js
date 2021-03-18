62f7523c09825ed989e9ef14dc67b6375fbc5611
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-03-18 20:52:30 -0300

    feat: add menu icons

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -2,60 +2,41 @@ import { adminRoot } from './defaultValues';
 // import { UserRole } from "../helpers/authHelper"
 
 const data = [
-  {
-    id: 'gogo',
-    icon: 'iconsminds-air-balloon-1',
-    label: 'menu.gogo',
-    to: `${adminRoot}/gogo`,
-    // roles: [UserRole.Admin, UserRole.Editor],
-    subs: [
-      {
-        icon: 'simple-icon-paper-plane',
-        label: 'menu.start',
-        to: `${adminRoot}/gogo/start`,
-      },
-    ],
-  },
-  {
-    id: 'secondmenu',
-    icon: 'iconsminds-three-arrow-fork',
-    label: 'menu.second-menu',
-    to: `${adminRoot}/second-menu`,
-    subs: [
-      {
-        icon: 'simple-icon-paper-plane',
-        label: 'menu.second',
-        to: `${adminRoot}/second-menu/second`,
-      },
-    ],
-  },
-  {
-    id: 'blankpage',
-    icon: 'iconsminds-bucket',
-    label: 'menu.blank-page',
-    to: `${adminRoot}/blank-page`,
-  },
+  // {
+  //   id: 'gogo',
+  //   icon: 'iconsminds-air-balloon-1',
+  //   label: 'menu.gogo',
+  //   to: `${adminRoot}/gogo`,
+  //   // roles: [UserRole.Admin, UserRole.Editor],
+  //   subs: [
+  //     {
+  //       icon: 'simple-icon-paper-plane',
+  //       label: 'menu.start',
+  //       to: `${adminRoot}/gogo/start`,
+  //     },
+  //   ],
+  // },
   {
     id: 'mesas',
-    icon: 'iconsminds-receipt',
+    icon: 'iconsminds-record-2',
     label: 'Mesas',
     to: `${adminRoot}/pedidos`,
   },
   {
     id: 'historico',
-    icon: 'iconsminds-receipt',
+    icon: 'iconsminds-box-with-folders',
     label: 'Historico de Caixa',
     to: `${adminRoot}/historico`,
   },
   {
     id: 'clientes',
-    icon: 'iconsminds-receipt',
+    icon: 'iconsminds-user',
     label: 'Clientes',
     to: `${adminRoot}/clientes`,
   },
   {
     id: 'caixa',
-    icon: 'iconsminds-receipt',
+    icon: 'iconsminds-cash-register-2',
     label: 'Caixa',
     to: `${adminRoot}/caixa`,
   },
